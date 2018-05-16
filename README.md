dd6a2d3d24898a3075f0b47162c5224f5c43d7b6
James Shakespeare <j@jshakespeare.com>
2018-05-16 10:59:59 -0400

    get nearest stations based on lat long

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Citibike Texts
+
+Receive a text with the number of bikes in your two nearest New York Citibike docks.
--- server.js
+++ server.js
@@ -4,28 +4,43 @@ import twilio from 'twilio';
 
 dotenv.config();
 
-const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, RECIPIENT_PHONE, SENDER_PHONE } = process.env;
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, RECIPIENT_PHONE, SENDER_PHONE, LATITUDE, LONGITUDE, NUM_STATIONS } = process.env;
 
-const url = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json';
+const stationInfoUrl = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json';
+const stationStatusUrl = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json';
 const client = new twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-// array of stations to check, taken from https://gbfs.citibikenyc.com/gbfs/en/station_information.json
-const stations = {
-  3414: {
-    name: 'Bergen/Flatbush',
-  },
-  3537: {
-    name: 'Carlton/Dean',
-  }
-};
+const findStations = () => {
+  return request(stationInfoUrl)
+    .then(json => {
+      const { data } = JSON.parse(json);
+      // sort the array by proximity to our lat/long
+      data.stations.sort((a, b) => {
+        const aLat = parseFloat(a.lat);
+        const aLong = parseFloat(a.lon);
+        const bLat = parseFloat(b.lat);
+        const bLong = parseFloat(b.lon);
+        const originLat = parseFloat(LATITUDE);
+        const originLong = parseFloat(LONGITUDE);
+        return getDistance(aLat, aLong, originLat, originLong) > getDistance(bLat, bLong, originLat, originLong) ? 1 : -1;
+      });
+      // slice the n nearest stations
+      const nearest = data.stations.slice(0, NUM_STATIONS);
+      // Return them as an object for easier indexing
+      return nearest.reduce((obj, current, index) => {
+        obj[parseInt(current.station_id)] = current;
+        return obj;
+      }, {});
+    });
+}
 
-function makeRequest() {
-  request(url)
+const queryStations = stations => {
+  return request(stationStatusUrl)
     .then(json => {
       const { data, last_updated } = JSON.parse(json);
       const updateTime = new Date(last_updated * 1000);
       // merge the returned data onto our array of stations
-      data.stations.map(station => {
+      const stationsWithData = data.stations.map(station => {
         const { station_id } = station;
         if (typeof stations[station_id] === 'undefined') {
           return;
@@ -33,29 +48,20 @@ function makeRequest() {
         // merge the response data into our station object
         stations[station_id] = Object.assign(stations[station_id], station);
       });
-      sendText(getMessage(stations, updateTime));
+      return {
+        stations,
+        updateTime,
+      };
     });
 }
 
 const getMessage = (stations, updateTime) => {
   const ids = Object.keys(stations);
-  return ids.reduce((str, id, index) => {
+  return ids.reduceRight((str, id, index) => {
     const { name, num_bikes_available } = stations[id];
-    const glue = index < ids.length - 1 ? index === ids.length - 2 ? ' and' : ',' : '.';
-    return `${str} ${num_bikes_available} bikes at ${name}${glue}`;
-  }, `${getGreeting()} As of ${updateTime.toLocaleTimeString()} there are`);
+    return `${str} ${num_bikes_available} bike${num_bikes_available === 1 ? '' : 's'} at ${name}${index ? ',' : '.'}`;
+  }, `As of ${updateTime.toLocaleTimeString()} there are`);
 };
-
-const getGreeting = () => {
-  const hour = new Date().getHours();
-  if(hour < 12) {
-    return 'Good morning!';
-  }
-  else if (hour < 18) {
-    return 'Good afternoon!';
-  }
-  return 'Good evening!';
-}
 
 const sendText = body => {
   return client.messages.create({
@@ -65,6 +71,17 @@ const sendText = body => {
   })
     .then(message => console.log(`Message sent to ${RECIPIENT_PHONE} from ${SENDER_PHONE} with ID ${message.sid}. Message body: "${body}"`))
     .catch(err => console.error(`Message not sent to ${RECIPIENT_PHONE} from ${SENDER_PHONE}. Error message returned: "${err.message}" (${err.code})`));
-}
+};
 
-makeRequest();
+const getDistance = (lat1, lon1, lat2, lon2) => {
+  const radlat1 = Math.PI * lat1 / 180
+  const radlat2 = Math.PI * lat2 / 180
+  const theta = lon1-lon2
+  const radtheta = Math.PI * theta / 180
+  const dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  return Math.acos(dist)
+};
+
+findStations()
+  .then(queryStations)
+  .then(({ stations, updateTime }) => sendText(getMessage(stations, updateTime)));
